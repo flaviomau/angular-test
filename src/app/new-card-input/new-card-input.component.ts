bff928356df4539f8d68e76b40cfb98effe6a60f
flaviomau <flaviomau@yahoo.com>
2019-06-04 21:07:19 -0300

    change form implementation

--- src/app/new-card-input/new-card-input.component.ts
+++ src/app/new-card-input/new-card-input.component.ts
@@ -1,35 +1,51 @@
-import { Component, EventEmitter, OnInit, Output, HostListener, ViewChild } from '@angular/core';
-import {NgForm} from '@angular/forms';
+import { Component, EventEmitter, OnInit, OnDestroy, Output, HostListener, /*ViewChild*/ } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { takeWhile, debounceTime, filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-new-card-input',
   templateUrl: './new-card-input.component.html',
   styleUrls: ['./new-card-input.component.scss'],
-  host: {'class': 'col-4'}
+  host: { 'class': 'col-4' }
 })
 
-export class NewCardInputComponent implements OnInit {
-  public newCard: any = {text: ''};
+export class NewCardInputComponent implements OnInit, OnDestroy {
+  @Output() onCardAdd = new EventEmitter<string>();
+  private alive = true;
 
-  @ViewChild('form', null) public form: NgForm;
+  newCardForm: FormGroup;
 
   @HostListener('document:keypress', ['$event'])
   handleKeyboardEvent(event: KeyboardEvent) {
-    if (event.code === "Enter" && this.form.valid) {
-      this.addCard(this.newCard.text);
+    if (event.code === "Enter" && this.newCardForm.valid) {
+      this.addCard(this.newCardForm.controls['text'].value);
     }
   }
 
-  @Output() onCardAdd = new EventEmitter<string>();
+  constructor(fb: FormBuilder) {
+    this.newCardForm = fb.group({
+      'text': ['', Validators.compose([Validators.required, Validators.minLength(2)])],
+    });
+
+    this.newCardForm.valueChanges.pipe(
+      filter((value) => this.newCardForm.valid),
+      debounceTime(500),
+      takeWhile(() => this.alive)
+    ).subscribe(data => {
+      console.log(data);
+    });
+  }
+
+  ngOnInit() {
+  }
 
   addCard(text) {
     this.onCardAdd.emit(text);
-    this.newCard.text = '';
+    this.newCardForm.controls['text'].setValue('');
   }
 
-  constructor() { }
-
-  ngOnInit() {
+  ngOnDestroy(){
+    this.alive = false;
   }
 
 }
